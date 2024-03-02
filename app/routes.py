e0d104c0760b3e075e17e52e99c9b7ab8fe6b0bb
aquajr <ayandaobosi@gmail.com>
2024-03-02 10:34:58 +0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from app import app, db
+from app.models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegisterForm
+from datetime import datetime
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form =LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect (url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Welcome {form.username.data}')
+        return redirect(url_for('login'))
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register URL"""
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have been registered successfully')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
